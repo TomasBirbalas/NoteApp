710593baefac02aeef376b1b0509960ea15c0699
tomasbir <tomas.birbalas@gmail.com>
2022-07-07 21:32:01 +0300

    Created DB Config

---
+++ NoteApp.Repository/DbConfig/DbConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NoteApp.Repository.DbConfig
+{
+    public class DbConfiguration : IDbConfiguration
+    {
+        public string ConnectionString { get; set; }
+        public DbContextOptions Options { get; set; }
+        public DbConfiguration()
+        {
+            ConnectionString = config.GetConnectionString("DatabaseConnect");
+            Options = new DbContextOptionsBuilder().UseSqlServer(ConnectionString).Options;
+        }
+
+    }
+}
---
+++ NoteApp.Repository/DbConfig/IDbConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NoteApp.Repository.DbConfig
+{
+    public interface IDbConfiguration
+    {
+        string ConnectionString { get; set; }
+        DbContextOptions Options { get; set; }
+    }
+}
