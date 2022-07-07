05ca1714b6a5a88f6bd999ede8d4213bfb242cee
tomasbir <tomas.birbalas@gmail.com>
2022-07-07 21:32:15 +0300

    Created database context

---
+++ NoteApp.Repository/DbContexts/NoteAppContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoteApp.Repository.DbConfig;
+using NoteApp.Repository.Entities;
+using NoteApp.Repository.Entities.NoteEntity;
+
+namespace NoteApp.Repository.DbContexts
+{
+    public class NoteAppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserDetails> UserDetails { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+        public NoteAppContext(IDbConfiguration options) : base(options.Options)
+        {
+
+        }
+    }
+}
