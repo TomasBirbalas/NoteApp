f34e4827ba5ab89d00afed4ae6057d42ad43077d
tomasbir <tomas.birbalas@gmail.com>
2022-07-12 20:50:13 +0300

    Update

--- NoteApp.WebAPI/Program.cs
+++ NoteApp.WebAPI/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using NoteApp.Business.Interfaces;
 using NoteApp.Business.Services;
 using NoteApp.Repository.DbConfigs;
 using NoteApp.Repository.DbContexts;
@@ -12,12 +13,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 builder.Services.AddScoped<IDbConfigurations, DbConfigurations>();
 
+//builder.Services.AddHttpClient<NoteServices>();
 builder.Services.AddScoped<IUserServices, UserServices>();
 builder.Services.AddScoped<ICategoryServices, CategoryServices>();
 builder.Services.AddScoped<INoteServices, NoteServices>();
-builder.Services.AddHttpClient<NoteServices>();
+
 builder.Services.AddDbContext<NoteAppContext>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
