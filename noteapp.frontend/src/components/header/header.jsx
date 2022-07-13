4ef690d63113f79b23e9390b7803838b3a326a76
tomasbir <tomas.birbalas@gmail.com>
2022-07-13 21:40:19 +0300

    Created FrontEnd project

---
+++ noteapp.frontend/src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Logo from './logo';
+
+
+class Header extends React.Component {
+    render() { 
+        return (
+            <header>
+                <Logo />
+            </header>
+        );
+    }
+}
+export default Header;
---
+++ noteapp.frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./stylesheets/css/main.min.css";
+
+import Header from "./components/header/header";
+
+ReactDOM.render(<Header />, document.getElementById("root"));
